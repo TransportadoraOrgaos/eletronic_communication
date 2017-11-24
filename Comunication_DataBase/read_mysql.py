b548d3317280f13edd3f7dea02356eadd414c693
Lucas Couto <loc.unb@gmail.com>
2017-11-24 17:41:56 -0200

    ModularizaÃÃo do cÃdigo em funÃÃes

--- Comunication_DataBase/read_mysql.py
+++ Comunication_DataBase/read_mysql.py
@@ -1,58 +1,45 @@
 import MySQLdb, requests, time
 from requests.exceptions import ConnectionError
 
+#Obtem o primeiro registro do banco de dados
+def get_db_data():
+    global db
+    db = MySQLdb.connect("localhost", "root", "root", "transorg")
+    global cursor
+    cursor = db.cursor() 
+    cursor.execute("SELECT * FROM transorg")
+    data = cursor.fetchone()
+    if not data:
+        print "Banco vazio"
+        time.sleep(3)
+        db.close()
+        return get_db_data()
+    else:
+        return data
 
-
-while True:
-
-    def get_db_data():
-
-        global db
-        db = MySQLdb.connect("localhost", "root", "root", "transorg")
-        global cursor
-        cursor = db.cursor()
-        
-        cursor.execute("SELECT * FROM transorg")
-        data = cursor.fetchone()
-
-        if not data:
-            print "Banco vazio"
-            time.sleep(3)
-            db.close()
-            return get_db_data()
-        else:
-            return data
-
-
-
+#Printa os valores do ultimo registro no terminal
+def print_data():
     data = get_db_data()
-
-
     print data[0] 
     print data[1]
 
-
-
+#Post to API
+def api_post():
     url = "https://transports-rest-api.herokuapp.com/reports"
     headers = {'content-type':'application/json'}
+    try:
+        response = requests.request("GET", url, headers=headers)
+        print(response.status_code)
+    except ConnectionError as e:
+        print("tentando bostar...")
+        time.sleep(5)
+        return api_post()
 
-    def api_post():
-        try:
-            response = requests.request("GET", url, headers=headers)
-            print(response.status_code)
-        except ConnectionError as e:
-            print("tentando bostar...")
-            time.sleep(5)
-            return api_post()
-
-
-    api_post()
-
-
-
+#Delete data
+def delete_data():
+    data = get_db_data()
     sql = 'DELETE FROM transorg WHERE date = ' + "'" + str(data[0]) + "'"
     print(sql)
-
     try:
         cursor.execute(sql)
         db.commit()
@@ -60,4 +47,9 @@ while True:
         db.rollback()
 
 
+while True:
+    get_db_data()
+    print_data()
+    api_post()
+    delete_data()
     db.close()
